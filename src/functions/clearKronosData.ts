d7558becff6197b00ebc113aa6f604a55b73faf8
damokless <damokless@protonmail.com>
2025-01-07 21:33:13 +0100

    fix : add if condition to clearKronosData func

--- src/functions/clearKronosData.ts
+++ src/functions/clearKronosData.ts
@@ -2,7 +2,9 @@ import KronosData from "../interface/IkronosData"
 
 export default async function clearKronosData(data: Array<KronosData>) : Promise<Array<KronosData>> {
     for (let index = 0; index < data.length; index += 2) {
-        data[index + 1].date = data[index].date
+        if (data[index].payCode === " Horaires") {
+            data[index + 1].date = data[index].date
+        }
     }
     return data.filter((item: KronosData) => item.payCode === " Horaires de badgeage")
 }
