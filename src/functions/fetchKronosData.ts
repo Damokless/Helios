b7d04bf7db670aca47aff8f1bb9ec67ae2519f92
damokless <damokless@protonmail.com>
2025-01-07 18:28:53 +0100

    fix : await on fetch func

--- src/functions/fetchKronosData.ts
+++ src/functions/fetchKronosData.ts
@@ -4,7 +4,7 @@ import KronosData from "../interface/IkronosData";
 export default async function fetchKronosData(startValue: CalendarDate | null, endValue: CalendarDate | null): Promise<Array<KronosData>> {
     const headers = await chrome.storage.local.get("kronosHeaders");
     const url = await chrome.storage.local.get("kronosUrl");
-    const kronosData = fetch(url.kronosUrl, {
+    const kronosData = await fetch(url.kronosUrl, {
         method: 'post',
         headers: headers.kronosHeaders,
         body: `sessionName=AdminSession&controller=MobileScheduleDisplayController&method=getViewList&params=%7B%22startDate%22:%22${startValue?.day}-${startValue?.month}-${startValue?.year}%22,%22endDate%22:%22${endValue?.day}-${endValue?.month}-${endValue?.year}%22%7D`,
