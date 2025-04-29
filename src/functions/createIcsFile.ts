b013f2388347c92574abb7607f40f807f7e5aaec
Damokless <damokless@protonmail.com>
2025-04-29 21:32:14 +0200

    feat: adding input to put a custom name to events

--- src/functions/createIcsFile.ts
+++ src/functions/createIcsFile.ts
@@ -2,7 +2,7 @@ import moment from 'moment'
 import KronosData from '../interface/IkronosData'
 import * as ics from 'ics'
 
-export default function createIcsFile(schedule: KronosData[]) {
+export default function createIcsFile(schedule: KronosData[], title : string) {
     const eventsArray: ics.EventAttributes[] = []
 
     schedule.forEach((element) => {
@@ -17,7 +17,7 @@ export default function createIcsFile(schedule: KronosData[]) {
         }
 
         eventsArray.push({
-            title: 'Disney 🏰🐭',
+            title: title || 'Disney 🏰🐭',
             start: [
                 startDate.year(),
                 startDate.month() + 1,
@@ -36,8 +36,6 @@ export default function createIcsFile(schedule: KronosData[]) {
     })
 
     const { error, value } = ics.createEvents(eventsArray)
-
-    console.log(value)
 
     if (!error) {
         return value
