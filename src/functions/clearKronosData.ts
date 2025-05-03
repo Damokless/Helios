8924bdcfcb542de61e11b49541e6d2295185c17c
Damokless <damokless@protonmail.com>
2025-05-03 16:19:11 +0200

    adding regex for different timestamp and accept shifts from EPC

--- src/functions/clearKronosData.ts
+++ src/functions/clearKronosData.ts
@@ -1,9 +1,12 @@
 import KronosData from '../interface/IkronosData'
 
 export default async function clearKronosData(data: Array<KronosData>): Promise<Array<KronosData>> {
+
     for (let index = 0; index < data.length; index++) {
+        const regex = /(?<=\s)\d*$/gm
+
         if (data[index].payCode === ' Horaires') {
-            data[index].time = data[index + 1].time.replace(' 404504', '')
+            data[index].time = data[index + 1].time.replace(regex, '') !== '' ? data[index].time.replace(regex, '') : data[index + 1].time.replace(regex, '')
         }
     }
 
