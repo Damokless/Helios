2759111c8fc5806733ddbd52e51f109a518af8d6
Damokles <damokless@protonmail.com>
2025-05-29 12:19:47 +0200

    fix : change logic to a OR logic instead of a ternary

--- src/functions/clearKronosData.ts
+++ src/functions/clearKronosData.ts
@@ -1,12 +1,12 @@
 import KronosData from '../interface/IkronosData'
 
 export default async function clearKronosData(data: Array<KronosData>): Promise<Array<KronosData>> {
+    const regex = /(?<=\s)\d*$/gm
 
     for (let index = 0; index < data.length; index++) {
-        const regex = /(?<=\s)\d*$/gm
 
         if (data[index].payCode === ' Horaires') {
-            data[index].time = data[index + 1].time.replace(regex, '') !== '' ? data[index].time.replace(regex, '') : data[index + 1].time.replace(regex, '')
+            data[index].time = data[index + 1].time.replace(regex, '') || data[index].time.replace(regex, '')
         }
     }
 
